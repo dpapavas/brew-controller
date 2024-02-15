8e43fee3bb843e0cd84a5f39d0ae5b5fa71fc463
Dimitris Papavasiliou <dimitris@debian>
2024-02-15 13:08:00 +0200

    Add copyright notices.

--- scheme/enclosure.scm
+++ scheme/enclosure.scm
@@ -1,5 +1,20 @@
 ;; -*- mode:scheme; coding: utf-8 -*-
 
+;; Copyright 2024 Dimitris Papavasiliou
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (import (srfi 1) (srfi 166) (scheme inexact)
         (gamma inexact)
         (gamma transformation) (gamma polygons) (gamma polyhedra)
